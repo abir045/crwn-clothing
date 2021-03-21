a1a0204b32fc326006eb2a4e7ad933f36faeb3c8
Salekin Imran <abir045@gmail.com>
2021-03-21 19:11:43 +0600

    updated color

--- client/src/global.styles.js
+++ client/src/global.styles.js
@@ -5,6 +5,15 @@ import { createGlobalStyle } from 'styled-components';
 body {
   font-family: "Open Sans Condensed";
   padding: 20px 60px;
+  background: #79a9bc;
+    /* fallback for old browsers */
+  
+  /* background: -webkit-linear-gradient(to right, #2ebf91, #8360c3);  */
+   /* Chrome 10-25, Safari 5.1-6 */
+  /* background: linear-gradient(to right, #2ebf91, #8360c3); */
+   /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+   
+ 
 
 
   @media screen and (max-width: 800px){
